3111809e0dc5d779872fea045f2f1e08828b289a
David <Huanzhen.Zhu@students.olin.edu>
2013-08-22 00:44:20 -0400

    Created the skeleton.

---
+++ engine.py
@@ -0,0 +1,16 @@
+# The main code for my Deal or No Deal game.
+# This code is meant of ease of reading.
+# It is broken down into several systematic steps that are well-documented.
+
+
+# IMPORTS
+
+# DECLARATIONS
+
+# RANDOMIZER
+
+# INPUT
+
+# DEALER
+
+# ENDGAME
